286f79f247f9a1b75a98cca3966d479ad82e18b2
이경민 <cow970814@naver.com>
2022-07-11 14:55:25 +0900

    chore : 앱 기본 설정

--- CustomKeyboard/ViewController.swift
+++ CustomKeyboard/ViewController.swift
@@ -2,16 +2,14 @@
 //  ViewController.swift
 //  CustomKeyboard
 //
+//  Created by 이경민 on 2022/07/11.
+//
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
     }
-
-
 }
-
