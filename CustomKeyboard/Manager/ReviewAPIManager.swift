aeb4fda71afce026407b6164774f8aa6f556542a
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-07-12 14:20:06 +0900

    [init] project init

---
+++ CustomKeyboard/Manager/ReviewAPIManager.swift
@@ -0,0 +1,19 @@
+//
+//  ReviewAPIManager.swift
+//  CustomKeyboard
+//
+//  Created by 백유정 on 2022/07/12.
+//
+
+import Foundation
+
+class ReviewAPIManager{
+    
+    func getReview() {
+        
+    }
+    
+    func postReview() {
+        
+    }
+}
