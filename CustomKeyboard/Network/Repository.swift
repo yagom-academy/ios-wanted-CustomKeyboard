2a8b6c3857f4dcd7225dcab49c609b49ece61239
신의연 <dusdl14@gmail.com>
2022-07-12 18:11:03 +0900

    [fix] baseUrl 고정

--- CustomKeyboard/Network/Repository.swift
+++ CustomKeyboard/Network/Repository.swift
@@ -13,9 +13,6 @@ enum RepositoryError {
 
 class Repository {
     
-    private var themeId = "PLKEY0-L-81"
-    private var currentPage = 0
-    
     private let httpClient = HttpClient(baseUrl: "https://api.plkey.app/theme/review?themeId=PLKEY0-L-81&start=0&count=20")
     
     func reviewData(completion: @escaping (Result<ModelData, Error>) -> Void) {
