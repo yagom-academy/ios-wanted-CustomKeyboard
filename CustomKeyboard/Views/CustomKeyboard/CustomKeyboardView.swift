8ec24ebd8de94e3fb1ee2f4ff24c444bd29d200c
wooooozin <wooooozin@gmail.com>
2022-07-12 21:07:08 +0900

    [feat] CustomKeyboardView Xib 파일 생성 및 버튼 IBOulet연결

---
+++ CustomKeyboard/Views/CustomKeyboard/CustomKeyboardView.swift
@@ -0,0 +1,47 @@
+//
+//  CustomKeyboardView.swift
+//  CustomKeyboard
+//
+//  Created by 효우 on 2022/07/12.
+//
+
+import UIKit
+
+class CustomKeyboardView: UIView {
+
+    @IBOutlet weak var qButton: UIButton!
+    @IBOutlet weak var wButton: UIButton!
+    @IBOutlet weak var eButton: UIButton!
+    @IBOutlet weak var rButton: UIButton!
+    @IBOutlet weak var tButton: UIButton!
+    @IBOutlet weak var yButton: UIButton!
+    @IBOutlet weak var uButton: UIButton!
+    @IBOutlet weak var iButton: UIButton!
+    @IBOutlet weak var oButton: UIButton!
+    @IBOutlet weak var pButton: UIButton!
+    
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var sButton: UIButton!
+    @IBOutlet weak var dButton: UIButton!
+    @IBOutlet weak var fButton: UIButton!
+    @IBOutlet weak var gButton: UIButton!
+    @IBOutlet weak var hButton: UIButton!
+    @IBOutlet weak var jButton: UIButton!
+    @IBOutlet weak var kButton: UIButton!
+    @IBOutlet weak var lButton: UIButton!
+    
+    @IBOutlet weak var shiftButton: UIButton!
+    @IBOutlet weak var zButton: UIButton!
+    @IBOutlet weak var xButton: UIButton!
+    @IBOutlet weak var cButton: UIButton!
+    @IBOutlet weak var vButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+    @IBOutlet weak var nButton: UIButton!
+    @IBOutlet weak var mButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var spaceButton: UIButton!
+    @IBOutlet weak var returnButton: UIButton!
+    
+}
