8f4a2a78bdbd1400495c26e46e7e1059e06824d6
kimkirim <kirimrlfla@naver.com>
2022-07-12 15:03:49 +0900

    [feat] 초기화면 연결 #5

--- CustomKeyboard/Presentation/ViewController.swift
+++ CustomKeyboard/Presentation/ViewController.swift
@@ -7,18 +7,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-//        NetworkManager.shared.fetchReview { result in
-//            switch result {
-//            case .success(let data):
-//                print(data)
-//            case .failure(let error):
-//                print(error.description)
-//            }
-//        }
+
         
     }
 }
