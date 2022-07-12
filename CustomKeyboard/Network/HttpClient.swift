a268277098cb7e900a4ae7795984613599a83e2b
신의연 <dusdl14@gmail.com>
2022-07-12 17:03:26 +0900

    [feat] 네트워크 통신 구현 및 레이어 나눔 #2

--- CustomKeyboard/Network/HttpClient.swift
+++ CustomKeyboard/Network/HttpClient.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 신의연 on 2022/07/12.
 //
-
+// URLSesstion으로 할지 멘토님한테 질문
 import Foundation
 
 class HttpClient {
@@ -13,21 +13,15 @@ class HttpClient {
     init(baseUrl: String) {
         self.baseUrl = baseUrl
     }
-
-    func getJson(completed: @escaping (Result<String, Error>) -> Void) {
+    // ?themeId=6&start=0&count=20
+    func getJson(params: [String: Any], completed: @escaping (Result<Data, Error>) -> Void) { // paging 질문
         DispatchQueue.global(qos: .background).async {
-            let queryParams = params.map { k, v in "\(k)=\(v)" }.joined(separator: "&")
-
-            var fullPath = path.hasPrefix("http") ? path : self.baseUrl + path
-            if !queryParams.isEmpty {
-                fullPath += "?" + queryParams
-            }
-
+            
             do {
-                let url = URL(string: fullPath)
-                let json = try String(contentsOf: url!, encoding: .utf8)
+                let url = URL(string: self.baseUrl)
+                let data = try Data(contentsOf: url!)
                 DispatchQueue.main.async {
-                    completed(Result.success(json))
+                    completed(Result.success(data))
                 }
             } catch {
                 DispatchQueue.main.async {
@@ -36,4 +30,22 @@ class HttpClient {
             }
         }
     }
+    func getJson(completed: @escaping (Result<Data, Error>) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            
+            do {
+                let url = URL(string: self.baseUrl)
+                let data = try Data(contentsOf: url!)
+                DispatchQueue.main.async {
+                    completed(Result.success(data))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completed(Result.failure(error))
+                }
+            }
+        }
+    }
+    
+    
 }
