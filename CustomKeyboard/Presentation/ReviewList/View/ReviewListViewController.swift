90250bb47774393794cabaf5054645ea0f4f2469
seocho <whtjdqls617@gmail.com>
2022-07-13 12:14:25 +0900

    [Feat]: 시간 차이 계산해서 표시

--- CustomKeyboard/Presentation/ReviewList/View/ReviewListViewController.swift
+++ CustomKeyboard/Presentation/ReviewList/View/ReviewListViewController.swift
@@ -21,12 +21,15 @@ class ReviewListViewController: BaseViewController {
         super.viewDidLoad()
         reviewListView.tableView.dataSource = self
         getDataFromServer()
+        resisterGestrueRecognizer()
         
-        // test
+//        postDataToServer()
+    }
+    
+    func resisterGestrueRecognizer() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(pressReviewInput))
         reviewListView.reviewInputLabel.isUserInteractionEnabled = true
         reviewListView.reviewInputLabel.addGestureRecognizer(tap)
-//        postDataToServer()
     }
     
     func getDataFromServer() {
@@ -60,9 +63,47 @@ extension ReviewListViewController: UITableViewDataSource {
         cell.profileImage.image = makeStringToImage(reviewList?.data[indexPath.row].user.profileImage ?? "")
         cell.userNameLabel.text = reviewList?.data[indexPath.row].user.userName
         cell.reviewTextLabel.text = reviewList?.data[indexPath.row].content
-        cell.timeLabel.text = reviewList?.data[indexPath.row].createdAt
+        cell.timeLabel.text = makeTimeLine(reviewList?.data[indexPath.row].createdAt ?? "")
+//        makeTimeLine(reviewList?.data[indexPath.row].createdAt ?? "")
         
         return cell
+    }
+    
+    func makeTimeLine(_ time : String) -> String {
+        // 우선 현재 시간이랑 차이를 구해야함
+        
+        // 현재 시간
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd hh"
+        let current = formatter.string(from: Date()).split(separator: " ")
+        let currentDate = current[0]
+        let currentHour = current[1]
+        
+        // 주어진 시간
+        let splitReviewDate = time.split(separator: "T")
+        let reviewDate = splitReviewDate[0]
+        let reviewTime = splitReviewDate[1]
+        let splitReviewTime = reviewTime.split(separator: ":")
+        let reviewHour = splitReviewTime[0]
+        let reviewMinute : String = {
+            if splitReviewTime[1].first == "0" {
+                return String(splitReviewTime[1].dropFirst())
+            } else {
+                return String(splitReviewTime[1])
+            }
+        }()
+        
+        if currentDate != reviewDate {
+            return String(reviewDate)
+        } else { // 하루 이내
+            if currentHour != reviewHour {
+                // 분 단위 표시
+                 return ("\(reviewMinute)분 전")
+            } else {
+                // 시간 단위 표시
+                 return ("\(reviewHour)시간 전")
+            }
+        }
     }
     
     func makeStringToImage(_ imageString : String) -> UIImage? {
