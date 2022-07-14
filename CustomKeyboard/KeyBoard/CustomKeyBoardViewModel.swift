478e792067822287937f535c011ec8160f4313c6
kimkirim <kirimrlfla@naver.com>
2022-07-15 08:40:30 +0900

    [fix] 빈문자열이 인자로 들어올때 에러 해결 #34

--- CustomKeyboard/KeyBoard/CustomKeyBoardViewModel.swift
+++ CustomKeyboard/KeyBoard/CustomKeyBoardViewModel.swift
@@ -18,11 +18,8 @@ struct CustomKeyBoardViewModel {
     func addWord(inputUniCode: Int, to beforeText: String?) -> String {
         guard let beforeText = beforeText else { return "" }
         let inputChar = String(UnicodeScalar(inputUniCode)!)
-        if beforeText == "" {
-            return inputChar
-        }
-        let lastCharUnicode = getLastCharUnicode(from: beforeText)
-        let beforeTextExceptLastChar = getStringExceptLastChar(from: beforeText)
+        guard let lastCharUnicode = getLastCharUnicode(from: beforeText),
+              let beforeTextExceptLastChar = getStringExceptLastChar(from: beforeText) else { return "" }
         let lastChar = engine.addWord(inputUniCode: inputUniCode, lastUniCode: lastCharUnicode)
         return beforeTextExceptLastChar + lastChar
     }
@@ -36,25 +33,24 @@ struct CustomKeyBoardViewModel {
     func removeWord(from beforeText: String?) -> String {
         //TODO: 지우기버튼 구현
         guard let beforeText = beforeText else { return "" }
-        if beforeText == "" {
-            return ""
-        }
-        let lastCharUnicode = getLastCharUnicode(from: beforeText)
-        let beforeTextExceptLastChar = getStringExceptLastChar(from: beforeText)
+        guard let lastCharUnicode = getLastCharUnicode(from: beforeText),
+              let beforeTextExceptLastChar = getStringExceptLastChar(from: beforeText) else { return "" }
+        
         let lastChar = engine.removeWord(lastUniCode: lastCharUnicode)
         
         return beforeTextExceptLastChar + lastChar
     }
     
     // 맨마지막 단어 유니코드 추출
-    private func getLastCharUnicode(from text: String) -> Int {
-        guard text != "" else { return 0 }
+    private func getLastCharUnicode(from text: String) -> Int? {
+        guard text != "" else { return nil }
         let lastChar = String(text.last!)
         return Int(UnicodeScalar(lastChar)!.value)
     }
     
     // 맨마지막 단어를 뺀 문자열 반환
-    private func getStringExceptLastChar(from text: String) -> String {
+    private func getStringExceptLastChar(from text: String) -> String? {
+        guard text != "" else { return nil }
         let i = text.index(text.endIndex, offsetBy: -1)
         return String(text[text.startIndex..<i])
     }
