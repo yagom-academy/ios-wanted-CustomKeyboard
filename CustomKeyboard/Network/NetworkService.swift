2e13968210255d5fb7d3b6588e75f50c8c0a9935
dongeunshin <7017410@naver.com>
2022-07-14 18:13:19 +0900

    [#6] chore: datatask with URLRequest로 변경

--- CustomKeyboard/Network/NetworkService.swift
+++ CustomKeyboard/Network/NetworkService.swift
@@ -13,15 +13,18 @@ protocol Api{
 
 class NetworkService{
     func request(){
-//        fetchListAll()
-        postRequest()
+        fetchListAll()
+//        postRequest()
     }
     private func fetchListAll(){
         let urlString = "https://api.plkey.app/theme/review?themeId=PLKEY0-L-81&start=0&count=2"
         guard let url = URL(string: urlString) else { return }
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-        let dataTask = session.dataTask(with: url) { (data, response, error) in
+        var requestURL = URLRequest(url: url)
+        requestURL.httpMethod = "GET"
+        requestURL.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession(configuration: .default)
+        let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
             guard error == nil else { return }
             let successsRange = 200..<300
             guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
@@ -30,6 +33,7 @@ class NetworkService{
             do{
                 let resultData = try JSONDecoder().decode(reviewList.self, from: data)
                 print("success")
+//                print(resultData)
             }catch{
                 print("ERROR: ", error)
             }
@@ -48,12 +52,13 @@ class NetworkService{
         requestURL.addValue("application/json", forHTTPHeaderField: "Content-Type")
         requestURL.httpBody = jsonData
 
-        let dataTask = URLSession.shared.dataTask(with: requestURL) { (data, response, error) in
+        let session = URLSession(configuration: .default)
+        let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
             guard error == nil else { return }
             let successsRange = 200..<300
             guard let statusCode = (response as? HTTPURLResponse)?.statusCode, successsRange.contains(statusCode)
             else { return }
-            print(statusCode)
+//            print(statusCode)
         }
         dataTask.resume()
     }
