6aa2bee9503664c115a7cb8c333f3ae93a9b42d8
prettyturtle <unifconti@gmail.com>
2022-07-14 11:17:21 +0900

    refactor: 유니코드 더하는 메서드 구현

---
+++ CustomKeyboard/View + ViewModel/Keyboard/SejongState.swift
@@ -0,0 +1,16 @@
+//
+//  SejongState.swift
+//  CustomKeyboard
+//
+//  Created by yc on 2022/07/14.
+//
+
+import Foundation
+
+enum SejongState {
+    case writeInitialState
+    case writeMiddleState
+    case writeLastState
+    case alreadyLastState
+    case alreadyDoubleLastState
+}
