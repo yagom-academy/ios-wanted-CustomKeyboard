6159b2768892355f9b8a7044e81289933dce405b
신의연 <dusdl14@gmail.com>
2022-07-19 17:43:31 +0900

    [refact] 한글 키보드 데이터로 음소 정보 이전 #36
    keyboareMaker에 있던 음소 state 정보 이전

--- CustomKeyboard/Hangul/HangulKeyboardData.swift
+++ CustomKeyboard/Hangul/HangulKeyboardData.swift
@@ -9,23 +9,33 @@ import Foundation
 
 struct HangulKeyboardData {
     
+    enum HangulState: Int {
+        case empty = 0
+        case cho = 1
+        case doubleCho = 2
+        case jung = 3
+        case doubleJung = 4
+        case jong = 5
+        case doubleJong = 6
+    }
+    
     var hangul: String = ""
     var unicode: Int = 0
-    var bornState: KeyboardMaker.HangulState = .empty
+    var bornState: HangulState = .empty
     
-    init(char: String, state: KeyboardMaker.HangulState) {
+    init(char: String, state: HangulState) {
         self.hangul = char
         self.unicode = convertStr2Unicode(char: char)
         self.bornState = state
     }
     
-    init(uni: Int, state: KeyboardMaker.HangulState) {
+    init(uni: Int, state: HangulState) {
         self.unicode = uni
         self.hangul = convertUni2Str(uni: uni)
         self.bornState = state
     }
     
-    init(char: String, uni: Int, lastState: KeyboardMaker.HangulState) {
+    init(char: String, uni: Int, lastState: HangulState) {
         self.hangul = char
         self.unicode = uni
         self.bornState = lastState
