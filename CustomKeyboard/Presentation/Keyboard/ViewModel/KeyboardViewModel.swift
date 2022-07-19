0e67ddfd59bd4e2d618235240e2688c64a7801c8
seocho <whtjdqls617@gmail.com>
2022-07-19 15:14:52 +0900

    [Feat] ViewModel 파일 추가

---
+++ CustomKeyboard/Presentation/Keyboard/ViewModel/KeyboardViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  KeyboardViewModel.swift
+//  CustomKeyboard
+//
+//  Created by 조성빈 on 2022/07/19.
+//
+
+import Foundation
---
+++ CustomKeyboard/Presentation/ReviewList/ViewModel/ReviewListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ReviewListViewModel.swift
+//  CustomKeyboard
+//
+//  Created by 조성빈 on 2022/07/19.
+//
+
+import Foundation
