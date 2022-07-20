1c9131c3075e5f6d4c0ecc0f49e5f724a5838e60
Kei <vvv.devk@gmail.com>
2022-07-20 12:06:34 +0900

    [test] XCTest case 추가
    - HangulAutomata deleteBuffer() 확인
    - HangulAutomata state 확인

--- CustomKeyboardAutomataTests/CustomKeyboardAutomataTests.swift
+++ CustomKeyboardAutomataTests/CustomKeyboardAutomataTests.swift
@@ -19,7 +19,7 @@ class CustomKeyboardAutomataTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testCompositHangul() throws {
         hangulAutomata.hangulAutomata(key: "ㄱ")
         hangulAutomata.hangulAutomata(key: "ㅏ")
         hangulAutomata.hangulAutomata(key: "ㅂ")
@@ -28,6 +28,28 @@ class CustomKeyboardAutomataTests: XCTestCase {
             $0 + $1
         }
         XCTAssertEqual("값", buffer)
+    }
+    
+    func testDeleteHangul() {
+        hangulAutomata.hangulAutomata(key: "ㄱ")
+        hangulAutomata.hangulAutomata(key: "ㅏ")
+        hangulAutomata.hangulAutomata(key: "ㅂ")
+        hangulAutomata.hangulAutomata(key: "ㅅ")
+        hangulAutomata.deleteBuffer()
+        let buffer = hangulAutomata.buffer.reduce("") {
+            $0 + $1
+        }
+        XCTAssertEqual("갑", buffer)
+    }
+    
+    func testAutomataStatus() {
+        hangulAutomata.hangulAutomata(key: "ㄱ")
+        hangulAutomata.hangulAutomata(key: "ㅏ")
+        hangulAutomata.hangulAutomata(key: "ㅂ")
+        hangulAutomata.hangulAutomata(key: "ㅅ")
+        let state = hangulAutomata.currentHangulState
+        
+        XCTAssertEqual(.dJongsung, state)
     }
 
     func testPerformanceExample() throws {
