76be9c87f925537c3644574254a781fa03d12a34
yoonju lee <rsyjlee@gmail.com>
2022-07-20 11:49:10 +0900

    [#14] feat: 이미지 캐싱 기능 구현

--- CustomKeyboard/Network/ProfileImageProvider.swift
+++ CustomKeyboard/Network/ProfileImageProvider.swift
@@ -14,10 +14,15 @@ protocol ProfileImageProviderType {
     
 }
 
-class ProfileImageProvider {
+class ProfileImageProvider: ProfileImageProviderType {
     
-    private var networkRequester: NetworkRequesterType?
+    private var networkRequester: NetworkRequesterType
     private var profileImageTask: URLSessionDataTask?
+    private var imageCache = NSCache<NSString, UIImage>()
+    
+    init(networkRequester: NetworkRequesterType) {
+        self.networkRequester = networkRequester
+    }
     
     func fetchImage(
         from urlString: String,
@@ -26,12 +31,20 @@ class ProfileImageProvider {
         if let task = profileImageTask {
             task.cancel()
         }
-        profileImageTask = networkRequester?.request(to: urlString) { result in
+
+        let cacheKey = NSString(string: urlString)
+        if let cachedImage = imageCache.object(forKey: cacheKey) {
+            completion(.success(cachedImage))
+            return
+        }
+
+        profileImageTask = networkRequester.request(to: urlString) { [weak self] result in
             switch result {
             case .success(let data):
                 guard let image = UIImage(data: data) else {
                     return
                 }
+                self?.imageCache.setObject(image, forKey: cacheKey)
                 completion(.success(image))
             case .failure(let error):
                 completion(.failure(error))
