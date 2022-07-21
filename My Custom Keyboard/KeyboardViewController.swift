f5e01d1deb9da47272118c48e0537be4af87a3da
wooooozin <wooooozin@gmail.com>
2022-07-21 09:08:14 +0900

    [feat] CustomKeyboard 기능 세팅

---
+++ My Custom Keyboard/KeyboardViewController.swift
@@ -0,0 +1,54 @@
+//
+//  KeyboardViewController.swift
+//  My Custom Keyboard
+//
+//  Created by 효우 on 2022/07/21.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    override func viewWillLayoutSubviews() {
+        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+    }
+
+}
