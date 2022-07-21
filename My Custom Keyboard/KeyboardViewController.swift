60e33f3ac9d666c1ce88572d4c0068cf6c3fb10a
wooooozin <wooooozin@gmail.com>
2022-07-21 10:13:41 +0900

    [feat] Extension Keyboard 적용 완료

--- My Custom Keyboard/KeyboardViewController.swift
+++ My Custom Keyboard/KeyboardViewController.swift
@@ -8,47 +8,53 @@
 import UIKit
 
 class KeyboardViewController: UIInputViewController {
+    
+    private let keyboardManager = HangulKeyboardManager()
 
     @IBOutlet var nextKeyboardButton: UIButton!
     
-    override func updateViewConstraints() {
-        super.updateViewConstraints()
-
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let customKeyboardView = Bundle.main.loadNibNamed("CustomKeyboard", owner: nil)?.first as? CustomKeyboardView else { return }
+        customKeyboardView.delegate = self
+        keyboardManager.delegate = self
+
+        guard let inputView = inputView else { return }
+
+        inputView.addSubview(customKeyboardView)
+        customKeyboardView.translatesAutoresizingMaskIntoConstraints = false
         
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .system)
+        NSLayoutConstraint.activate([
+            customKeyboardView.leftAnchor.constraint(equalTo: inputView.leftAnchor),
+            customKeyboardView.topAnchor.constraint(equalTo: inputView.topAnchor),
+            customKeyboardView.rightAnchor.constraint(equalTo: inputView.rightAnchor),
+            customKeyboardView.bottomAnchor.constraint(equalTo: inputView.bottomAnchor)
+        ])
         
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-        
-        self.view.addSubview(self.nextKeyboardButton)
-        
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
     }
     
     override func viewWillLayoutSubviews() {
-        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
         super.viewWillLayoutSubviews()
     }
-    
-    override func textDidChange(_ textInput: UITextInput?) {
-        
-        var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-            textColor = UIColor.white
-        } else {
-            textColor = UIColor.black
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
-    }
+}
 
+extension KeyboardViewController: KeyboardInfoReceivable, HangulKeyboardDataReceivable {
+    func customKeyboardView(pressedKeyboardButton: UIButton) {
+        guard let textData = pressedKeyboardButton.titleLabel?.text else { return }
+        keyboardManager.enterText(text: textData)
+    }
+    func hangulKeyboard(enterPressed: HangulKeyboardData) {
+        self.dismissKeyboard()
+    }
+    
+    func hangulKeyboard(updatedResult text: String) {
+        while self.textDocumentProxy.hasText {
+            self.textDocumentProxy.deleteBackward()
+        }
+        self.textDocumentProxy.insertText(text)
+    }
 }
