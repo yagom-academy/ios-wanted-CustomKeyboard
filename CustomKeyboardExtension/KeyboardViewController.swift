7ca72430b323d9fe3eb6b2f959d3c5fcd632a978
이경민 <cow970814@naver.com>
2022-07-21 12:52:51 +0900

    feat : 테스트 코드 케이스 추가 및 테스트 실행

--- CustomKeyboardExtension/KeyboardViewController.swift
+++ CustomKeyboardExtension/KeyboardViewController.swift
@@ -4,14 +4,52 @@
 //
 //  Created by 이경민 on 2022/07/20.
 //
+// 1번 지워야 하는 경우 - false
+
+/// 최후의 보루
+//keyboardView.viewModel.result.bind { value in
+//    if let documentCount = proxy.documentContextBeforeInput?.count {
+//        for i in 0..<documentCount {
+//            proxy.deleteBackward()
+//        }
+//    }
+//    proxy.insertText(value)
+//}
+
+
+// 2번 지워야 하는 경우 - true
+    // 1. 이중 종성이 있을때, 모음이 들어 온 경우
+    // 2. 단종성이 있을 때, 모음이 들어 온 경우 - compat - next // 안녕핫 -> 안녕하세
+
+// 지우면 안되는 경우 - nil
+    // 1. 이중 종성이 있을 경우, 초성이 들어온 경우 - compat - next
+    // 2. 단종성이 있을 경우, 이중 종성이 안되는 초성이 들어온 경우
+    // 5. 단중성이 있을 때, 이중 중성이 안되는 경우
+    // 6. 이중 중성이 있을때, 중성이 들어온 경우
+    // 7. 초성이 있을때, 초성이 들어온 경우 - compat - next
 
 import UIKit
 
+//enum DeleteCase {
+//    case once // 한번 지우는 경우 (기본값)
+//    case twice // 두번 지우는 경우
+//    case zero // 지우면 안되는 경우
+//}
+
+// ㅁ + ㅜ -> 2개 지우고 => 글자 1개 넣고
+// 무 + 궁 -> 2개 지우고 => 글자가 1개라면  2개 넣고
+// 무궁 + 화 -> 2개 지우고 => 글자가 1 3개 넣고
+// 무궁화 + 삼 -> 2개 지우고 3개 넣고
+// 무궁화삼 + 천 -> 2개 지우고 3개 넣고
+
+
+// ㅁ ㅜ 무궁
 class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
     let viewModel = KeyboardViewModel()
     lazy var keyboardView = KeyboardView(viewModel: viewModel)
+    var isDoubleDelete: Bool = false
     
     override func updateViewConstraints() {
         super.updateViewConstraints()
@@ -44,16 +82,16 @@ class KeyboardViewController: UIInputViewController {
         self.keyboardView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         self.keyboardView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         self.keyboardView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-
-        let proxy = self.textDocumentProxy
         
-        keyboardView.viewModel.result.bind { resultValue in
-            let result = resultValue.last
-            
-            if result != nil {
-                proxy.insertText(String.init(result!))
-            }
-        }
+//        let proxy = self.textDocumentProxy
+//        
+//        keyboardView.viewModel.result.bind { value in
+//            if !value.isEmpty {
+//                proxy.insertText(String(UnicodeScalar(Chosung.ㄱ.rawValue)!))
+//                proxy.insertText(String(UnicodeScalar(Jungsung.ㅏ.rawValue)!))
+//                proxy.insertText(String(UnicodeScalar(Jungsung.ㅣ.rawValue)!))
+//            }
+//        }
     }
     
     override func viewWillLayoutSubviews() {
@@ -62,6 +100,7 @@ class KeyboardViewController: UIInputViewController {
     }
     
     override func textWillChange(_ textInput: UITextInput?) {
+//        print(textInput)
         // The app is about to change the document's contents. Perform any preparation here.
     }
     
--- CustomKeyboardTests/CustomKeyboardTests.swift
+++ CustomKeyboardTests/CustomKeyboardTests.swift
@@ -34,4 +34,121 @@ class CustomKeyboardTests: XCTestCase {
         XCTAssertEqual(expected, viewModel.result.value)
     }
     
+    // 스페이스
+    func test_한칸띄어쓰기() {
+        viewModel.result.value = "헤일못"
+        
+        viewModel.addSpace()
+        
+        let expect = "헤일못 "
+        XCTAssertEqual(expect, viewModel.result.value)
+    }
+    
+    // 지우는 것
+    func test_지우기() {
+        let compats: [Compatibility] = [.ㅎ, .ㅔ, .ㅇ, .ㅣ, .ㄹ, .ㅁ, .ㅗ, .ㅂ, .ㅅ]
+        
+        compats.forEach { value in
+            viewModel.didTapKeyboardButton(buffer: value)
+        }
+        
+        (0..<3).forEach { _ in
+            viewModel.didTapBack()
+        }
+        
+        let expect = "헤일ㅁ"
+        XCTAssertEqual(expect, viewModel.result.value)
+    }
+    
+    func test_띄어쓰기가마지막에있는텍스트지우기() {
+        let compats: [Compatibility] = [.ㅎ, .ㅔ, .ㅇ, .ㅣ, .ㄹ, .ㅁ, .ㅗ, .ㅂ, .ㅅ]
+        
+        compats.forEach { value in
+            viewModel.didTapKeyboardButton(buffer: value)
+        }
+        viewModel.addSpace()
+        
+        let expect = 4
+        var resultCount = 0
+        while !viewModel.result.value.isEmpty {
+            viewModel.didTapBack()
+            resultCount += 1
+        }
+        
+        XCTAssertEqual(expect, resultCount)
+    }
+    
+
+    
+    func test_띄어쓰기가중간에있는텍스트지우기() {
+        let compats: [Compatibility?] = [.ㅎ, .ㅔ, nil, .ㅇ, .ㅣ, .ㄹ, .ㅁ, .ㅗ, .ㅂ, .ㅅ]
+        
+        compats.forEach { value in
+            if let compat = value{
+                viewModel.didTapKeyboardButton(buffer: compat)
+            } else {
+                viewModel.addSpace()
+            }
+            // 헤 일몺
+        }
+        
+        let expect = 9
+        var resultCount = 0
+        while !viewModel.result.value.isEmpty {
+            viewModel.didTapBack()
+            resultCount += 1
+        }
+        
+        XCTAssertEqual(expect, resultCount)
+    }
+    
+    func test_마지막을지웠다가다시적었을경우() { // 헤일못 -> 헤일몺
+        let compats: [Compatibility] = [.ㅎ, .ㅔ, .ㅇ, .ㅣ, .ㄹ, .ㅁ, .ㅗ, .ㅅ]
+        let afterCompats: [Compatibility] = [.ㅂ, .ㅅ]
+        compats.forEach { value in
+            viewModel.didTapKeyboardButton(buffer: value) // 헤일못
+        }
+        
+        viewModel.didTapBack()
+        
+        afterCompats.forEach { value in
+            viewModel.didTapKeyboardButton(buffer: value) // 헤일몺
+        }
+        let expect = "헤일몺"
+        
+        XCTAssertEqual(expect, viewModel.result.value)
+    }
+    
+    // 헤일못
+    func test_마지막스페이스를지우고다시작성하는경우() {
+        let compats: [Compatibility] = [.ㅎ, .ㅔ, .ㅇ, .ㅣ, .ㄹ, .ㅁ, .ㅗ, .ㅅ]
+        compats.forEach { value in
+            viewModel.didTapKeyboardButton(buffer: value)
+        }
+        
+        viewModel.addSpace() // 헤일못" "
+        viewModel.didTapBack() // 헤일못
+        
+        viewModel.didTapKeyboardButton(buffer: .ㄱ)
+        
+        let expect = "헤일못ㄱ"
+        
+        XCTAssertEqual(expect, viewModel.result.value)
+    }
+    
+    func test_마지막스페이스를지우고다시작성히하는데이중모음이가능한경우() { // 헤일몹" " => 헤일몹ㅅ
+        let compats: [Compatibility] = [.ㅎ, .ㅔ, .ㅇ, .ㅣ, .ㄹ, .ㅁ, .ㅗ, .ㅂ]
+        
+        compats.forEach { value in
+            viewModel.didTapKeyboardButton(buffer: value)
+        }
+        
+        viewModel.addSpace()
+        viewModel.didTapBack()
+        
+        viewModel.didTapKeyboardButton(buffer: .ㅅ)
+        let expect = "헤일몹ㅅ"
+        
+        XCTAssertEqual(expect, viewModel.result.value)
+    }
 }
